e5243b86ebd57308e2608bbaa6e0027512337e62
Blackscure <wekesabuyahi@gmail.com>
2020-06-27 23:11:10 +0300

    gettr and setter methods created

--- app/src/main/java/com/blackscure/travel_mantis/TraveDeals.java
+++ app/src/main/java/com/blackscure/travel_mantis/TraveDeals.java
@@ -14,4 +14,44 @@ public class TraveDeals {
         this.imageUrl = imageUrl;
         this.description = description;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
