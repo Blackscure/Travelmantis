35e36c62eb8a364aea2101a977deb2f02676bea4
Blackscure <wekesabuyahi@gmail.com>
2020-06-28 00:05:20 +0300

    get and setter methods created

--- app/src/main/java/com/blackscure/travel_mantis/TraveDeal.java
+++ app/src/main/java/com/blackscure/travel_mantis/TraveDeal.java
@@ -1,13 +1,13 @@
 package com.blackscure.travel_mantis;
 
-public class TraveDeals {
+public class TraveDeal {
     private String id;
     private String  title;
     private String price;
     private  String imageUrl;
     private String description;
 
-    public TraveDeals(String id, String title, String price, String imageUrl, String description) {
+    public TraveDeal(String title, String price, String imageUrl, String description) {
         this.id = id;
         this.title = title;
         this.price = price;
