9897eac7116ab16fe7ca4a3e4b49ed02127e3cec
Blackscure <wekesabuyahi@gmail.com>
2020-06-27 23:05:12 +0300

    contructore created in travelDeals file

--- app/src/main/java/com/blackscure/travel_mantis/TraveDeals.java
+++ app/src/main/java/com/blackscure/travel_mantis/TraveDeals.java
@@ -1,4 +1,17 @@
 package com.blackscure.travel_mantis;
 
 public class TraveDeals {
+    private String id;
+    private String  title;
+    private String price;
+    private  String imageUrl;
+    private String description;
+
+    public TraveDeals(String id, String title, String price, String imageUrl, String description) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
 }
